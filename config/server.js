9d60a0b1f20f8cf769d99950e7fcbb8654de35b4
serengen <serengensengen@gmail.com>
2022-11-28 23:12:28 -0300

    deployment

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://guitarla-server.herokuapp.com/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
