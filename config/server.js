c597fc55cca6e26435662c8bcedafa9e0b3b9ea6
serengen <serengensengen@gmail.com>
2022-11-28 23:23:19 -0300

    deployment

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', 'https://guitarla-server.herokuapp.com/'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('EkuDPMrq9aDRHGHyb3O9IQ==,WeHSiiRSQMGHAa3tx9i0xg==,SiGVVPTCGE9T7rB7dqJ11Q==,PEpezAMAfvLk1LePcSIj5Q=='),
+    keys: ['EkuDPMrq9aDRHGHyb3O9IQ==','WeHSiiRSQMGHAa3tx9i0xg==','SiGVVPTCGE9T7rB7dqJ11Q==','PEpezAMAfvLk1LePcSIj5Q=='],
   },
 });
