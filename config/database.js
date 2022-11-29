12f9f41177396a2b438134e2525be63718b0ebeb
serengen <serengensengen@gmail.com>
2022-11-29 00:12:12 -0300

    me quiero ir

--- config/database.js
+++ config/database.js
@@ -7,9 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'guitarla_hrhu'),
       user: env('DATABASE_USERNAME', 'serengen'),
       password: env('DATABASE_PASSWORD', '4NofoUQ5kTmHxwrGoPk8iEbZnomPo1ZV'),
-      options: {
-        ssl: false,
-      },
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
