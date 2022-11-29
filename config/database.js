f893da45fcd26dddc306cd41f2aa313ab773ce13
serengen <serengensengen@gmail.com>
2022-11-28 23:39:31 -0300

    deploymentas

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'guitarla_hrhu'),
       user: env('DATABASE_USERNAME', 'serengen'),
       password: env('DATABASE_PASSWORD', '4NofoUQ5kTmHxwrGoPk8iEbZnomPo1ZV'),
-      ssl: env.bool('DATABASE_SSL', true),
+      "ssl": { "rejectUnauthorized": false }
     },
   },
 });
