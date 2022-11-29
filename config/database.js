a84276d512133d11bd3d6c8f96773d23cc0059a0
serengen <serengensengen@gmail.com>
2022-11-28 22:52:39 -0300

    subiendo server

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'dpg-ce1n4pda4996ndtoo7ng-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'guitarla_hrhu'),
+      user: env('DATABASE_USERNAME', 'serengen'),
+      password: env('DATABASE_PASSWORD', '4NofoUQ5kTmHxwrGoPk8iEbZnomPo1ZV'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+  },
+});
