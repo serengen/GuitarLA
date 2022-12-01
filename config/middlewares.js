a1ef1a5b8ed8f72c1351a447024cd30e9e1727ef
Tomás Navarro <62296179+serengen@users.noreply.github.com>
2022-12-01 12:27:25 -0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,7 +9,7 @@ module.exports = [
           'connect-src': ["'self'", 'https:'],
           'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-          upgradeInsecureRequests: true,
+          upgradeInsecureRequests: null,
         },
       },
     },
